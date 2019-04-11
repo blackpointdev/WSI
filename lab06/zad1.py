9721db7c75e8b4300c71c5ddc53918a4659b1d61
Marcin Wiśnios <marcinw1998@gmail.com>
2019-04-11 23:56:35 +0200

    First commit

--- lab06/zad1.py
+++ lab06/zad1.py
@@ -2,30 +2,19 @@ import pymc3 as pm
 import numpy as np
 import matplotlib.pyplot as plt
 
-with pm.Model() as grypa_model:
-
+with pm.Model() as Bayes_model:
+    # zmienna mówiąca czy podejrzany jest winny, o rozkładzie Bernoulliego z prawd. 0.5
     zabil = pm.Bernoulli('zabil', 0.5)
 
-    # prawd. kataru pod warunkiem grypy
-    # jeżeli grypa to p_katar=0.5, jeżeli nie grypa to p_katar=0.3
-    p_katar = pm.Deterministic('p_katar', pm.math.switch(zabil, 0.5, 0.3))
-    # zmienna katar o rozkładie Bernoulliego
-    katar = pm.Bernoulli('katar', p_katar)
+    # prawdopodobieństwo znalezienia odcisków palców podejrzanego pod warunkiem że zabił
+    p_odciski = pm.Deterministic("p_odciski", pm.math.switch(0.7, 0.3))
+    odciski = pm.Bernoulli("odciski", p_odciski)
 
-    # prawd. kaszlu pod warunkiem grypy
-    # jeżeli grypa to p_kaszel=0.3, jeżeli nie grypa to p_kaszel=0.3
-    p_kaszel = pm.Deterministic('p_kaszel', pm.math.switch(grypa, 0.3, 0.3))
-    # zmienna kaszel o rozkładie Bernoulliego
-    kaszel = pm.Bernoulli('kaszel', p_kaszel)
+    p_alibi = pm.Deterministic("p_alibi", pm.math.switch(0.8, 0.4))
+    alibi = pm.Bernoulli("alibi", p_alibi)
 
-    # prawd. goraczki pod warunkiem grypy
-    # jeżeli grypa to p_goraczka=0.8, jeżeli nie grypa to p_goraczka=0.4
-    p_goraczka = pm.Deterministic('p_goraczka', pm.math.switch(grypa, 0.8, 0.4))
-    goraczka = pm.Bernoulli('goraczka', p_goraczka)
+    p_motyw = pm.Deterministic("p_motyw", pm.math.switch(0.9, 0.5))
+    motyw = pm.Bernoulli("motyw", p_alibi)
 
-with grypa_model:
-    trace = pm.sample(20000, chains=1)  #liczba symulacji, są inne parametry warto sprawdzić w dokumentacji
-
-# w module pymc3 dostępne są metody do wygernerowania różnego rodzaju wykresów np.
-axs = pm.traceplot(trace, varnames=['grypa', 'katar', 'kaszel', 'goraczka'])
-plt.show()
+    p_alibi = pm.Deterministic("p_alibi", pm.math.switch(0.8, 0.4))
+    alibi = pm.Bernoulli("alibi", p_alibi)
