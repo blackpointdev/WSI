ae0643387c21fb0f0ae9a9cad6a2fc3f36fb424a
Marcin Wiśnios <marcinw1998@gmail.com>
2019-04-23 20:49:06 +0200

    Added zad. 1

---
+++ lab07/zad1.py
@@ -0,0 +1,88 @@
+from fuzzython.fsets.triangular import Triangular
+from fuzzython.variable import Variable
+from fuzzython.adjective import Adjective
+from fuzzython.ruleblock import RuleBlock
+import numpy as np
+import matplotlib.pyplot as plt
+from fuzzython.systems.mamdani import MamdaniSystem
+
+from fuzzython.norms import norms
+
+
+# pomocnicza funkcja do rysowania zbiorów rozmytych
+def plot_fuzzyset(ax, fuzzy_set, x, *args, **kwargs):
+    y = np.array([fuzzy_set(e) for e in x])
+    ax.plot(x, y,  *args, **kwargs)
+    ax.set_ylim(-0.1, 1.1)
+    ax.legend()
+
+
+s_slow = Triangular((9.99, 0), (10, 1), (73.33, 0))
+s_medium = Triangular((10, 0), (73.33, 1), (136.66, 0))
+s_fast = Triangular((73.34, 0), (136.66, 1), (200, 0))
+s_very_fast = Triangular((136.66, 0), (200, 1), (200.1, 0))
+
+a_s_slow = Adjective("s_slow", s_slow)
+a_s_medium = Adjective("s_medium", s_medium)
+a_s_fast = Adjective("s_fast", s_fast)
+a_s_very_fast = Adjective("s_very_fast", s_very_fast)
+
+speed = Variable("speed", "km/h", a_s_slow, a_s_medium, a_s_fast, a_s_very_fast)
+
+v_poor = Triangular((0.04, 0), (0.05, 1), (1.975, 0))
+v_medium = Triangular((0.05, 0), (1.975, 1), (4, 0))
+v_good = Triangular((1.975, 0), (4, 1), (4.1, 0))
+
+a_v_poor = Adjective("v_poor", v_poor)
+a_v_medium = Adjective("v_medium", v_medium)
+a_v_good = Adjective("v_good", v_good)
+
+visibility = Variable("visibility", "km", a_v_poor, a_v_medium, a_v_good)
+
+a_very_low = Triangular((-0.01, 0), (0, 1), (0.33, 0))
+a_low = Triangular((0,0), (0.33, 1), (0.66, 0))
+a_medium = Triangular((0.33, 0), (0.66, 1), (1, 0))
+a_big = Triangular((0.66, 0), (1, 1), (1.1, 0))
+
+a_a_very_low = Adjective("a_very_low", a_very_low)
+a_a_low = Adjective("a_low", a_low)
+a_a_medium = Adjective("a_medium", a_medium)
+a_a_big = Adjective("a_big", a_big)
+
+accident = Variable("accident", "%", a_a_very_low, a_a_low, a_a_medium, a_a_big, defuzzification='COG', default=0)
+# wykresy poglądowe
+x = np.linspace(10,200,1000)
+x2 = np.linspace(0.05, 4, 1000)
+x3 = np.linspace(0, 1, 1000)
+fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(12,8))
+((ax1), (ax2), (ax3)) = axs
+plot_fuzzyset(ax1, s_slow, x, 'b', label='s_slow')
+plot_fuzzyset(ax1, s_medium, x, 'g', label='s_medium')
+plot_fuzzyset(ax1, s_fast, x, 'r', label='s_fast')
+plot_fuzzyset(ax1, s_very_fast, x, 'y', label='s_very_fast')
+plot_fuzzyset(ax2, v_poor , x2, 'b', label='v_poor')
+plot_fuzzyset(ax2, v_medium, x2, 'g', label='v_medium')
+plot_fuzzyset(ax2, v_good , x2, 'r', label='v_good')
+plot_fuzzyset(ax3, a_very_low, x3, 'b', label='a_very_low')
+plot_fuzzyset(ax3, a_low, x3, 'g', label='a_low')
+plot_fuzzyset(ax3, a_medium, x3, 'r', label='a_medium')
+plot_fuzzyset(ax3, a_big, x3, 'r', label='a_big')
+plt.show()
+
+scope = locals()
+
+rule1 = 'if speed is a_s_slow or visibility is a_v_good then accident is a_a_very_low'
+rule2 = 'if speed is a_s_very_fast or visibility is a_v_poor then accident is a_a_big'
+
+block = RuleBlock('rb_mamdani', operators=('MIN','MAX','ZADEH'), activation='MIN', accumulation='MAX')
+block.add_rules(rule1, rule2, scope=scope)
+
+mamdani = MamdaniSystem('mamdani_model', block)
+# dane wejściowe
+inputs = {'speed': 199, 'visibility': 0.1} #tak naprawdę to można podać liczby rzeczywiste od 0 do 10
+# obliczenie odpowiedzi
+res = mamdani.compute(inputs)
+#zwraca słownik
+print(res)
+
+
