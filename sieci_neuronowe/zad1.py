9a69798bebdc992a8cdbd4f44d787f073a6d9a63
Marcin Wiśnios <marcinw1998@gmail.com>
2019-05-29 14:07:51 +0200

    Added __init__ and predict methods

--- sieci_neuronowe/zad1.py
+++ sieci_neuronowe/zad1.py
@@ -4,13 +4,19 @@ import numpy as np
 class Perceptron:
     # Inicjalizator, ustawiający atrybut self.w jako wektor losowych wag, n ilość sygnałów wejściowych
     def __init__(self, n):
-        # YOUR CODE HERE
-        raise NotImplementedError()
+        self.w = np.random.randint(-1, 2, size=(1, n))
+        print(self.w)
 
     # Metoda obliczająca odpowiedz modelu dla zadanego sygnału wejściowego x=[1,x1,x2,...,xN]
     def predict(self, x):
-        # YOUR CODE HERE
-        raise NotImplementedError()
+        sum = 0
+        for i in range(len(x)):
+            sum += x[i] + self.w[i]
+
+        if sum > 0:
+            return True
+        else:
+            return False
 
     # Metoda uczenia według reguły perceptronu, xx - zbiór danych uczących, d - odpowiedzi,
     # eta - współczynnik uczenia,
@@ -24,3 +30,5 @@ class Perceptron:
     def evaluate_test(self, xx, d):
         # YOUR CODE HERE
         raise NotImplementedError()
+
+Perceptron(5)
